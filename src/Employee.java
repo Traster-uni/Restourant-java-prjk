e1e46b6ae87f58c6647b2885d1568953ef0fc3e4
Traster-uni <lopedote.1942920@studenti.uniroma1.it>
2021-07-20 16:43:30 +0200

    .

---
+++ src/Employee.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Tommaso M. Lopedote on 20/07/2021
+ * Time: 16:18
+ * Project: Restourant-java-prjk
+ */
+import java.util.ArrayList;
+
+
+public class Employee {
+    private int tavolo;
+    ArrayList<Plates> 
+
+}
