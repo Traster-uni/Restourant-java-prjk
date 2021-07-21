0e0fa9296cec18f6d3c81a25a1ec293ebeb9aafd
Traster-uni <lopedote.1942920@studenti.uniroma1.it>
2021-07-21 17:42:39 +0200

    added Plate and cheff
    added the 2 classes, cheff is being constructed

---
+++ src/Cheff.java
@@ -0,0 +1,38 @@
+import javax.swing.*;
+import java.util.ArrayList;
+
+/**
+ * Created by Tommaso M. Lopedote on 21/07/2021
+ * Time: 17:23
+ * Project: Restourant-java-prjk
+ */
+//class Cheff
+//	attributi:
+//        nome:
+//        bufferPlate: ArrayList di inserimenti
+//          private menuDirectory: directory del file di testo.
+//
+//     metodi:
+//      protected newPlate("namePlate", prezzo int): aggiunge una nuova instanza di Piatto all'interno dell'array
+//      protected changePlate( ) : modifica un piatto al menù.
+//      protected deletePlate( ): cancella un piatto dall' array.
+//      protected writeMenu( ): scrive su filepath i piatti inseriti in bufferPlate indicizzando gli inserimenti.
+//      protected readMenu( ): legge il file e compila bufferPlate per una eventuale modifica del file del menu.
+
+public class Cheff {
+    private String name;
+    private ArrayList<Plate> bufferPlate;
+    private String menuDirectory;
+
+    public Cheff(){
+        name = "";
+        bufferPlate = new ArrayList<>();
+        menuDirectory = "";
+    }
+
+    public Cheff(String newName, String directory){
+        this.name = newName;
+        bufferPlate = new ArrayList<>();
+        menuDirectory = directory;
+    }
+}
