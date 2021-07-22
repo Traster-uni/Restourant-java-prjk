daedee51e12e129a99a3630db7f40c4618e7a527
anastasiya1309 <baranovska01@mail.ru>
2021-07-22 18:40:22 +0200

    Ho aggiunto un nuovo metodo endOrder e ho scritto dei commenti

--- src/Waiter.java
+++ src/Waiter.java
@@ -1,18 +1,34 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Waiter extends Employee{
     private ArrayList<Plate> order;
 
+    /**
+     * Default constructor for Waiter object calls
+     * super class constructor by default with no arguments.
+     */
     public Waiter() {
         super();
         order = new ArrayList<>();
     }
 
+    /**
+     * Default constructor for Waiter object calls
+     * super class constructor by default.
+     * @param initialServedTable - the served table.
+     */
     public Waiter(Integer initialServedTable){
         super(initialServedTable);
         order = new ArrayList<>();
     }
 
+    /**
+     * Adds the dish to the order list.
+     * @param plateName - plate name
+     * @param category - selectable category
+     * @param restaurant - our restaurant
+     */
     public void addPlate(String plateName, Integer category, Restaurant restaurant){
         ArrayList<Plate> menu = restaurant.getMenuArray();
         for (int i = 0; i<menu.size(); i++){
@@ -24,6 +40,10 @@ public class Waiter extends Employee{
         }
     }
 
+    /**
+     * Delete the dish to the order list.
+     * @param plateName - plate name.
+     */
     public void deletePlate(String plateName){
         for (int i=0; i>order.size(); i++){
             Plate currentPlate = order.get(i);
@@ -34,7 +54,13 @@ public class Waiter extends Employee{
         }
     }
 
-    public void endOrder(){
-
+    /**
+     * Submit the order in the order table and cancel the order.
+     * @param restaurant - our restaurant
+     */
+    public void endOrder(Restaurant restaurant){
+        HashMap<Integer, ArrayList<ArrayList<Plate>>> orders = restaurant.getOrderDict();
+        orders.get(super.getServedTable()).add(order);
+        order.clear();
     }
 }
