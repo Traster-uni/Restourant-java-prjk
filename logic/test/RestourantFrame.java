7792b181a98ab244b0825f75aee408abd6abbbf7
Traster-uni <lopedote.1942920@studenti.uniroma1.it>
2021-08-19 12:13:52 +0200

    changed project structure to accomodate a new Graphical module

---
+++ logic/test/RestourantFrame.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+/**
+ * Created by Tommaso M. Lopedote on 18/08/2021
+ * Time: 12:02
+ * Project: Restourant-java-prjk
+ */
+public class RestourantFrame {
+    private JPanel Restourant;
+    private JButton button1;
+}
--- test/RestourantFrame.java
+++
@@ -1,7 +0,0 @@
-/**
- * Created by Tommaso M. Lopedote on 18/08/2021
- * Time: 12:02
- * Project: Restourant-java-prjk
- */
-public class RestourantFrame {
-}
