6371f4bb09ba71c77fb585a06b6afce9c503fa22
anastasiya1309 <baranovska01@mail.ru>
2021-09-10 19:54:24 +0200

    nella classe Frame sto testando Java Swing

---
+++ logic/src/Frame.java
@@ -0,0 +1,47 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Frame {
+
+
+//    public static void main(String[] args) {
+//        JFrame frame = new JFrame();
+//        frame.setSize(1000, 800);
+//        frame.setTitle("Restaurant");
+//        StartPanel startPanel = new StartPanel();
+//        MainMenuPanel mainMenuPanel = new MainMenuPanel();
+//        frame.setContentPane(startPanel.getStartPanel());
+//        JButton startButton = startPanel.getStartButton();
+//        startButton.addActionListener(new ActionListener() {
+//            @Override
+//            public void actionPerformed(ActionEvent e) {
+//                frame.setContentPane(mainMenuPanel.getMainMenuPanel());
+//            }
+//        });
+//
+//        frame.setVisible(true);
+//        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+//    }
+//public class StartPanel extends JPanel{
+//    private JButton startButton;
+//    private JPanel startPanel;
+//
+//    public StartPanel() {
+//        startPanel = new JPanel(new GridBagLayout());
+//        startPanel.add(startButton);
+//        startButton = new JButton("START");
+//        startButton.setPreferredSize(new Dimension(200, 60));
+//    }
+//
+//    public JPanel getStartPanel() {
+//        return startPanel;
+//    }
+//
+//    public JButton getStartButton() {
+//        return startButton;
+//    }
+//}
+
+}
