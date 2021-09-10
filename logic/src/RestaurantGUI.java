4a8e46de5c1a047aca5aef89126d65692dd522e2
Traster-uni <lopedote.1942920@studenti.uniroma1.it>
2021-09-10 20:02:05 +0200

    mainMenu added

---
+++ logic/src/RestaurantGUI.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+/**
+ * Created by Tommaso M. Lopedote on 10/09/2021
+ * Time: 19:59
+ * Project: Restourant-java-prjk
+ */
+public class RestaurantGUI {
+    private JButton startButton;
+    private JPanel panel1;
+}
