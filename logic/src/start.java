f49677e17d34dd54e9a73d80c9b1185d46d8c7c2
Traster-uni <lopedote.1942920@studenti.uniroma1.it>
2021-09-10 16:19:45 +0200

    basic jSwing module

---
+++ logic/src/start.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+/**
+ * Created by Tommaso M. Lopedote on 10/09/2021
+ * Time: 16:16
+ * Project: Restourant-java-prjk
+ */
+public class start {
+    private JButton button1;
+    private JPanel panel1;
+}
