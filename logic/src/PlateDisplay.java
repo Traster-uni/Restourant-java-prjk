50c6978b39ba7b135b35097726bbe98afa65e5a7
anastasiya1309 <baranovska01@mail.ru>
2021-09-23 17:08:39 +0200

    Ho aggiornato plateDisplay

--- logic/src/PlateDisplay.java
+++ logic/src/PlateDisplay.java
@@ -1,20 +1,19 @@
 import javax.swing.*;
 import java.awt.*;
 
-/**
- * Created by Tommaso M. Lopedote on 18/09/2021
- * Time: 18:38
- * Project: Restourant-java-prjk
- */
 public class PlateDisplay extends JLabel implements ListCellRenderer<Plate> {
     public Component getListCellRendererComponent(JList<? extends Plate> list, Plate plate, int index,
                                                   boolean isSelected, boolean cellHasFocus) {
         if (isSelected){
-            setBackground(list.getSelectionBackground());
-            setForeground(list.getSelectionForeground());
+//            setBackground(list.getSelectionBackground());
+//            setForeground(list.getSelectionForeground());
+            setBackground(new Color(115, 133, 142, 75));
+            setForeground(new Color(68, 74, 79));
         }else{
-            setBackground(list.getBackground());
-            setForeground(list.getForeground());
+//            setBackground(list.getBackground());
+//            setForeground(list.getForeground());
+            setBackground(Color.BLACK);
+            setForeground(Color.BLACK);
         }
         setFont(new Font("Comic Sans", Font.PLAIN, 20));
         setText(plate.getName() + " " + plate.getPrize() + " Euro" );
